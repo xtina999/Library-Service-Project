079201f6864624ae3d1fc7b22dd1f0254f633036
xtina999 <avgustroder@gmail.com>
2024-06-20 22:02:58 +0300

    Implement list & detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from .models import Borrowing
+from .serializers import BorrowingSerializer
+from book.models import Book
 
-# Create your views here.
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = [permissions.IsAdminUser]
+
