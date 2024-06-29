22e15bac1977a5d970c4cc53d509d4356cbb79d8
xtina999 <avgustroder@gmail.com>
2024-06-29 22:01:31 +0300

    Edited sending messages to Telegram via HTTP request

--- borrowings/utils.py
+++ borrowings/utils.py
@@ -1,19 +1,36 @@
-import os
 import requests
 from dotenv import load_dotenv
+import os
+import logging
+
+from telegram import Bot
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+logger = logging.getLogger(__name__)
 
 
 def send_telegram_message(message):
-    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+    # bot = Bot(token=TELEGRAM_TOKEN)
+    # bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': message,
+        'parse_mode': 'HTML'
     }
-    response = requests.post(url, data=payload)
-    return response.json()
+
+    logger.info(f"Sending message to Telegram: {message}")
+
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        logger.info(f"Response: {response.status_code}, {response.json()}")
+    except requests.RequestException as e:
+        logger.error(f"Error sending Telegram message: {e}")
+        return None
+
+    return response
